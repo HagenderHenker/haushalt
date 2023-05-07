58ba7391d2e24b200a85eb9222aa828c0a0b3f66
hagenderhenker <md1234567890@web.de>
2023-05-07 13:22:07 +0200

    plotting graph a create dicts for printing erg

--- graphplotter.py
+++ graphplotter.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as gp
 import dataimport as di
 import pathlib
 import datetime as dt
-
+import matplotlib.pyplot as plt
 
 def gr_populationdevelopment(xlsfile, gde, hhj):
     data = di.readewstatistik_wohn(xlsfile=xlsfile, gdenr=gde, jahr=hhj-1)
@@ -17,6 +18,28 @@ def gr_populationdevelopment(xlsfile, gde, hhj):
     dn = dn.set_index("jahr", drop=True)
     return dn
 
+"""
+def plot_gr_popdev(xlsfile, gde, hhj):
+    data = gr_populationdevelopment(xlsfile=xlsfile, gde=gde, hhj=hhj)
+    fig = px.line(data_frame=data             )
+    fig.show()
+"""
 
-df = gr_populationdevelopment(xlsfile=str(pathlib.Path.cwd() / "hhdaten/grunddaten.xlsx"), gde=60, hhj=2023)
-print(df)
+def plot_gr_popdev(xlsfile, gde, hhj):
+    data = gr_populationdevelopment(xlsfile=xlsfile, gde=gde, hhj=hhj)
+    
+    plt.plot(data['gesamt'], color='blue', marker='o')
+    plt.title('Bevölkerungsentwicklung', fontsize=14)
+    plt.xlabel('Jahr', fontsize=12)
+    plt.yticks(np.arange(3000, 5000, step = 1000))
+    plt.ylabel('Anzahl Einwohner', fontsize=12)
+    plt.grid(True)
+
+
+    for year in np.sort(data.index):
+        plt.annotate(str(data.loc[year]["gesamt"]), xy=(year, data.loc[year]["gesamt"]), xytext=(year, (data.loc[year]["gesamt"])-200))
+
+   
+    plt.savefig(str(pathlib.Path.cwd() / "hhdaten/plots/bev-entw.png"))
+
+plot_gr_popdev(xlsfile=str(pathlib.Path.cwd() / "hhdaten/grunddaten.xlsx"), gde=60, hhj=2023)
--- haushalt.py
+++ haushalt.py
@@ -1,9 +1,11 @@
+import dataimport as di 
 import contextbuilder
-import pathlib 
+import pathlib
 
 gde = 60
 hhj = 2023
 grunddaten = str(pathlib.Path.cwd() / "hhdaten/grunddaten.xlsx")
+
 bewegungsdaten = str(pathlib.Path.cwd() / "hhdaten/bewegungsdaten.xlsx")
 
 
@@ -12,3 +14,9 @@ if __name__ == "__main__":
     #
     contexthhsatzung = contextbuilder.hhsatzung(gde = gde, hhj = hhj, xlsgrunddaten = grunddaten, xlsbewegung = bewegungsdaten)
     print(contexthhsatzung)
+
+    dfbew = di.hhdata_excelimport(xlsxfile=bewegungsdaten)
+
+    dferl = di.erl_excelimport(xlsxfile=bewegungsdaten)
+
+    dfgde = di.readgrunddatengde(xlsfile=grunddaten, gdenr=gde, hhj=hhj)
